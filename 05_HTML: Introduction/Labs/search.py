d2b580682dcf4536afebf16e34c6e7ad40201ee4
Rihad Variawa <rihadv021@gmail.com>
2024-07-16 21:42:26 +0200

    html lab

--- 05_HTML: Introduction/Labs/search.py
+++ 05_HTML: Introduction/Labs/search.py
@@ -11,21 +11,21 @@ with open('page.html', 'w') as web_page:
     web_page.write('</tr>')
 
     #Print All Records
-    for line in file[1:]:
-        web_page.write('<tr>')
-        record = line.split(',')
-        for value in record:
-            web_page.write(f'<td>{value}</td>')
-        web_page.write('</tr>')
+    # for line in file[1:]:
+    #     web_page.write('<tr>')
+    #     record = line.split(',')
+    #     for value in record:
+    #         web_page.write(f'<td>{value}</td>')
+    #     web_page.write('</tr>')
 
     # #Print Records Based on Value
-    # for line in file[1:]:
-    #     line = line.replace('\n','')
-    #     record = line.split(',')
-    #     if record[4] == 'Pollen':
-    #         web_page.write('<tr>')
-    #         for value in record:
-    #             web_page.write(f'<td>{value}</td>')
-    #     web_page.write('</tr>')
+    for line in file[1:]:
+        line = line.replace('\n','')
+        record = line.split(',')
+        if record[4] == 'Pollen':
+            web_page.write('<tr>')
+            for value in record:
+                web_page.write(f'<td>{value}</td>')
+        web_page.write('</tr>')
 
     web_page.write('<table>')
