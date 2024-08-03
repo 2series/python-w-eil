c4b6b314bddd88565baec2394d944aa85278e125
Rihad Variawa <rihadv021@gmail.com>
2024-08-03 15:00:38 +0200

    Updated json-parse.py and json-pretty.py files with new code and comments, adding demos for retrieving IP address information and country information using REST APIs

--- 08_Python - REST API's, Requests and JSON/Code Examples/json-parse.py
+++ 08_Python - REST API's, Requests and JSON/Code Examples/json-parse.py
@@ -1,14 +1,41 @@
-# script respond output dict format
+# script retrieves information about a specific IP address using the ip-api.com API and prints the result in a pretty JSON format
 # dumps module Response is prettier
 
 from requests import get
 from json import dumps
 
-ip = '151.101.3.5' #cnn.com
+# ip = '151.101.3.5' #cnn.com
 
-result = get(f'http://ip-api.com/json/{ip}').json()
+# result = get(f'http://ip-api.com/json/{ip}').json()
+
 
 # print(result) # response dict of values
 # print(result['city']) # response San Francisco
 
-print(dumps(result, indent=2))
+# print(dumps(result, indent=2))
+
+
+# Demo
+# script retrieves the driver and constructor standings from the Ergast API and prints the result in a pretty JSON format
+
+# Set the API endpoints and parameters
+driver_standings_endpoint = 'http://ergast.com/api/f1/current/driverStandings.json'
+constructor_standings_endpoint = 'http://ergast.com/api/f1/current/constructorStandings.json'
+
+# Make the API requests
+driver_standings_response = get(driver_standings_endpoint)
+constructor_standings_response = get(constructor_standings_endpoint)
+
+# Check if the requests were successful
+if driver_standings_response.status_code == 200 and constructor_standings_response.status_code == 200:
+    # Parse the JSON responses
+    driver_standings = driver_standings_response.json()
+    constructor_standings = constructor_standings_response.json()
+
+    # Print the standings in a pretty JSON format
+    print("Driver Standings:")
+    print(dumps(driver_standings, indent=2))
+    print("\nConstructor Standings:")
+    print(dumps(constructor_standings, indent=2))
+else:
+    print(f"Failed to retrieve standings. Status codes: {driver_standings_response.status_code}, {constructor_standings_response.status_code}")
--- 08_Python - REST API's, Requests and JSON/Code Examples/json-pretty.py
+++ 08_Python - REST API's, Requests and JSON/Code Examples/json-pretty.py
@@ -4,14 +4,45 @@
 from requests import get
 from json import dumps
 
-result = get('https://restcountries.com/v3.1/name/belgium').json()
+# Sending GET Request
+# result = get('https://restcountries.com/v3.1/name/mexico').json()
 
 # print(result)
 
-print(dumps(result, indent=2))
+# print(dumps(result, indent=2))
 
+# Accessing Specific Data (Commented Out)
 # print(result[0]['name']['official'])
 # print(result[0]['capital'])
 # print(result[0]['capital'][0])
 
 # print(result[0]['name']['nativeName']['deu']['official'])
+
+# Demo
+# Building a Country Information Application
+# A simple command-line application that allows users to retrieve information about a specific country using the REST Countries API. We'll use the script you provided as a starting point and add some features to make it more interactive
+
+def get_country_info(country_name):
+    try:
+        response = get(f'https://restcountries.com/v3.1/name/{country_name}').json()
+        return response[0]
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+def print_country_info(country_info):
+    if country_info:
+        print(f"Official Name: {country_info['name']['official']}")
+        print(f"Capital City: {country_info['capital'][0]}")
+        print(f"Population: {country_info['population']}")
+        print("Native Languages:")
+        for language in country_info['languages'].values():
+            print(f"- {language}")
+
+def main():
+    country_name = input("Enter a country name: ")
+    country_info = get_country_info(country_name)
+    print_country_info(country_info)
+
+if __name__ == "__main__":
+    main()
