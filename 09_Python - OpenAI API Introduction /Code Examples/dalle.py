7ee8f0aa174447e4224debd344cf6669f82c3d39
Rihad Variawa <rihadv021@gmail.com>
2024-08-07 18:46:35 +0200

    Added API key loading from .env file and updated image size to 1024x1024

--- 09_Python - OpenAI API Introduction /Code Examples/dalle.py
+++ 09_Python - OpenAI API Introduction /Code Examples/dalle.py
@@ -1,14 +1,26 @@
+
+# script uses the OpenAI API to generate an image based on a given prompt using the DALL-E 3 model
+
+import os
+from dotenv import load_dotenv
 from openai import OpenAI
 
-client = OpenAI()
+# Load the .env file
+load_dotenv()
+
+# Get the OpenAI API key from the .env file
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+# Initialize the OpenAI client with the API key
+openai.api_key = openai_api_key
 
 query = 'a dog on the moon'
 
 response = client.images.generate(
   model="dall-e-3",
   prompt=query,
-  n=1, #How many images to return
-  size="512x512" #256x256, 512x512, 1024x1024
+  n=1, # Number of images to return. meaning only one image will be generated
+  size="1024x1024" # 1024x1024
 )
 print(response)
 print('')
