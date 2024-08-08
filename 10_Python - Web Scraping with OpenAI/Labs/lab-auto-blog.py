c942a0c5d6522e439cb1055e37a7703af53fdb28
Rihad Variawa <rihadv021@gmail.com>
2024-08-08 22:12:59 +0200

    Code formatting changes: added and removed comment lines, no functional changes

--- 10_Python - Web Scraping with OpenAI/Labs/lab-auto-blog.py
+++ 10_Python - Web Scraping with OpenAI/Labs/lab-auto-blog.py
@@ -5,75 +5,81 @@
 
 # Overall, this script is designed to automate the process of generating blog posts based on articles from an RSS feed
 
-import os
-from dotenv import load_dotenv
-import feedparser
-from bs4 import BeautifulSoup
-from requests import get
-from openai import OpenAI
+# import os
+# from dotenv import load_dotenv
+# import feedparser
+# from bs4 import BeautifulSoup
+# from requests import get
+# from openai import OpenAI
 
-# Load environment variables from .env file
-load_dotenv()
+# # Load environment variables from .env file
+# load_dotenv()
 
-# Get the OpenAI API key from the environment variable
-openai_key = os.environ['OPENAI_API_KEY']
+# # Get the OpenAI API key from the environment variable
+# openai_key = os.environ['OPENAI_API_KEY']
 
-# Create an OpenAI client instance with the API key
-client = OpenAI(api_key=openai_key)
+# # Create an OpenAI client instance with the API key
+# client = OpenAI(api_key=openai_key)
 
 
-def build_list(url):
-    rss_feed = get(url).text
-    feed = feedparser.parse(rss_feed)
+# def build_list(url):
+#     rss_feed = get(url).text
+#     feed = feedparser.parse(rss_feed)
 
-    url_list = []
-    for post in feed['entries']:
-        url_list.append(post['link'])
+#     url_list = []
+#     for post in feed['entries']:
+#         url_list.append(post['link'])
 
-    return url_list
+#     return url_list
 
-def scrape(url):
-    page = get(url).text
-    soup = BeautifulSoup(page, 'html.parser')
-    post = soup.find_all('p')
-    text =''
-    for line in post: #Create a string without HTML tags
-        text = f'{text} {line.text}'
-    return text
+# def scrape(url):
+#     page = get(url).text
+#     soup = BeautifulSoup(page, 'html.parser')
+#     post = soup.find_all('p')
+#     text =''
+#     for line in post: #Create a string without HTML tags
+#         text = f'{text} {line.text}'
+#     return text
 
-def ai(query):
-    completion_post = client.chat.completions.create(
-    model="gpt-4o",
-    messages=[
-        {"role": "system", "content": "You are a Blogger."},
-        {"role": "user", "content": f"Write a 100 word blog post on -- {query}"},
-        {"role": "user", "content": f"Do not mention author"},
-        {"role": "user", "content": f"Do not mention when post was written"},
-    ]
-    )
-    response_post = completion_post.choices[0].message.content
+# def ai(query):
+#     completion_post = client.chat.completions.create(
+#     model="gpt-4o",
+#     messages=[
+#         {"role": "system", "content": "You are a Blogger."},
+#         {"role": "user", "content": f"Write a 100 word blog post on -- {query}"},
+#         {"role": "user", "content": f"Do not mention author"},
+#         {"role": "user", "content": f"Do not mention when post was written"},
+#     ]
+#     )
+#     response_post = completion_post.choices[0].message.content
 
-    completion_title = client.chat.completions.create(
-    model="gpt-4o",
-    messages=[
-        {"role": "system", "content": "You are a Blogger."},
-        {"role": "user", "content": f"Create a Title for a blog post about -- {response_post}"},
-    ]
-    )
-    response_title = completion_title.choices[0].message.content
+#     completion_title = client.chat.completions.create(
+#     model="gpt-4o",
+#     messages=[
+#         {"role": "system", "content": "You are a Blogger."},
+#         {"role": "user", "content": f"Create a Title for a blog post about -- {response_post}"},
+#     ]
+#     )
+#     response_title = completion_title.choices[0].message.content
     
-    return response_title, response_post
+#     return response_title, response_post
 
-url = 'https://feeds.arstechnica.com/arstechnica/index'
+# url = 'https://feeds.arstechnica.com/arstechnica/index'
 
-url_list = build_list(url)
+# url_list = build_list(url)
 
-for page in url_list[:5]: #[:5] limits the loop to 5 iterations
-    response_bs = scrape(page)
-    response_ai = ai(response_bs)
+# for page in url_list[:5]: #[:5] limits the loop to 5 iterations
+#     response_bs = scrape(page)
+#     response_ai = ai(response_bs)
 
-    print(f'{response_ai[0]}\n {response_ai[1]}')
+#     print(f'{response_ai[0]}\n {response_ai[1]}')
 
-    with open('auto-blog.html', 'a') as file:
-        file.write(f'<h1>{response_ai[0]}</h1>')
-        file.write(f'<p>{response_ai[1]}<p>')
+#     with open('auto-blog.html', 'a') as file:
+#         file.write(f'<h1>{response_ai[0]}</h1>')
+#         file.write(f'<p>{response_ai[1]}<p>')
+        
+        
+    
+    
+    
+ # Local model
